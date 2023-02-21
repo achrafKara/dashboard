9d10b8ce6f0f40754e483f0e65ee89d14b2ee524
AshRaff <achraf.karabila@outlook.com>
2023-02-21 14:44:30 +0100

    initial commit

---
+++ src/js/modules/font-awesome.js
@@ -0,0 +1,12 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { 
+    faCheck, 
+    faQrcode,
+    faFlag,
+    faStar,
+    faFileLines
+} from "@fortawesome/free-solid-svg-icons";
+
+
+library.add(faCheck, faQrcode, faFlag, faStar, faFileLines);
+dom.watch();
---
+++ src/js/modules/svg-map.js
@@ -0,0 +1,4 @@
+import svgMap from "svgmap";
+import "svgmap/dist/svgMap.min.css";
+
+window.svgMap = svgMap;
